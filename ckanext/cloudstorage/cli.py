adc32c2d576886ea4dc8a4d52356eb08e915a30f
JVickery-TBS <jesse.vickery@tbs-sct.gc.ca>
2023-04-20 19:03:21 +0000

    feat(commands): csv output and list-uploads;
    - Added `list-uploads` command.
    - Added output option.
    - Added csv file output.

--- ckanext/cloudstorage/cli.py
+++ ckanext/cloudstorage/cli.py
@@ -5,6 +5,8 @@ import os.path
 import cgi
 import tempfile
 import click
+import unicodecsv as csv
+from sqlalchemy import and_ as _and_
 
 from docopt import docopt
 from ckan.lib.cli import CkanCommand
@@ -21,6 +23,7 @@ from ckanext.cloudstorage.model import (
     drop_tables
 )
 from ckan.logic import NotFound
+from ckan.plugins.toolkit import h
 
 USAGE = """ckanext-cloudstorage
 
@@ -31,17 +34,20 @@ Commands:
     - list-unlinked-uploads     Lists uploads in the storage container that do not match to any resources.
     - remove-unlinked-uploads   Permanently deletes uploads from the storage container that do not match to any resources.
     - list-missing-uploads      Lists resources IDs that are missing uploads in the storage container.
+    - list-uploads              Lists uploads in the storage container that do match to a resource.
 
 Usage:
     cloudstorage fix-cors <domains>... [--c=<config>]
     cloudstorage migrate <path_to_storage> [<resource_id>] [--c=<config>]
     cloudstorage initdb [--c=<config>]
-    cloudstorage list-unlinked-uploads [--c=<config>]
+    cloudstorage list-unlinked-uploads [--o=<output>] [--c=<config>]
     cloudstorage remove-unlinked-uploads [--c=<config>]
-    cloudstorage list-missing-uploads [--c=<config>]
+    cloudstorage list-missing-uploads [--o=<output>] [--c=<config>]
+    cloudstorage list-uploads [--o=<output>] [--c=<config>]
 
 Options:
     -c=<config>       The CKAN configuration file.
+    -o=<output>       The output file path.
 """
 
 
@@ -55,6 +61,11 @@ class PasterCommand(CkanCommand):
     summary = 'ckanext-cloudstorage maintence utilities.'
     usage = USAGE
 
+    def __init__(self, name):
+        super(PasterCommand, self).__init__(name)
+        self.parser.add_option('-o', '--output', dest='output', action='store',
+                               default=None, help='The output file path.')
+
     def command(self):
         self._load_config()
         args = docopt(USAGE, argv=self.args)
@@ -66,11 +77,13 @@ class PasterCommand(CkanCommand):
         elif args['initdb']:
             _initdb()
         elif args['list-unlinked-uploads']:
-            _list_unlinked_uploads()
+            _list_unlinked_uploads(self.options.output)
         elif args['remove-unlinked-uploads']:
             _remove_unlinked_uploads()
         elif args['list-missing-uploads']:
-            _list_missing_uploads()
+            _list_missing_uploads(self.options.output)
+        elif args['list-uploads']:
+            _list_uploads(self.options.output)
 
 
 def _migrate(args):
@@ -179,45 +192,111 @@ def _fix_cors(args):
         )
 
 
-def _get_unlinked_uploads(return_objects = False):
+def _get_uploads(get_linked = True, return_upload_objects_only = False):
     cs = CloudStorage()
 
     resource_urls = set(os.path.join(
-                        'resources',
+                        u'resources',
                         id,
                         munge_filename(filename))
                     for id, filename in
                     model.Session.query(
                         model.Resource.id,
                         model.Resource.url) \
-                        .filter(model.Resource.url_type == 'upload') \
+                        .filter(model.Resource.url_type == u'upload') \
                         .all())
 
     uploads = cs.container.list_objects()
 
-    uploads_missing_resources = []
+    parsed_uploads = []
     for upload in uploads:
-        if upload.name not in resource_urls:
-            uploads_missing_resources.append(
-                upload if return_objects else upload.name)
+        if (upload.name in resource_urls
+            if get_linked else
+            upload.name not in resource_urls):
 
-    return uploads_missing_resources
+            if return_upload_objects_only:
+                parsed_uploads.append(upload)
+                continue
+
+            resource_id = upload.name.split('/')[1]
+            resource_fields = None
+            if resource_id:
+                resource_fields = model.Session.query(
+                                    model.Resource.id,
+                                    model.Resource.url,
+                                    model.Resource.package_id,
+                                    model.Resource.created,
+                                    model.Resource.last_modified,
+                                    model.Package.owner_org) \
+                                    .join(model.Package, model.Resource.package_id == model.Package.id) \
+                                    .filter(_and_(model.Resource.url_type == u'upload',
+                                                model.Resource.id == resource_id)) \
+                                    .first()
+            parsed_uploads.append({
+                u'resource_id': resource_fields[0] if resource_fields else None,
+                u'resource_filename': resource_fields[1] if resource_fields else None,
+                u'package_id': resource_fields[2] if resource_fields else None,
+                u'created': h.render_datetime(resource_fields[3]) if resource_fields else None,
+                u'last_modified': h.render_datetime(resource_fields[4]) if resource_fields else None,
+                u'organization_id': resource_fields[5] if resource_fields else None,
+                u'upload_url': upload.name,
+                u'upload_size': u"{} KB".format((upload.size / 1000.0))})
+
+    return parsed_uploads
 
 
-def _list_unlinked_uploads():
-    uploads_missing_resources = _get_unlinked_uploads()
+def _write_uploads_to_csv(output_path, uploads):
+    if not uploads:
+        click.echo(u"Nothing to write to {}".format(output_path))
+        return
+    with open(output_path, u'w') as f:
+        w = csv.writer(f, encoding='utf-8')
+        w.writerow((u'resource_id',
+                    u'package_id',
+                    u'organization_id',
+                    u'resource_filename',
+                    u'upload_url',
+                    u'upload_file_size',
+                    u'resource_created',
+                    u'resource_last_modified'))
+        for upload in uploads:
+            w.writerow((
+                upload[u'resource_id'],
+                upload[u'package_id'],
+                upload[u'organization_id'],
+                upload[u'resource_filename'],
+                upload[u'upload_url'],
+                upload[u'upload_size'],
+                upload[u'created'],
+                upload[u'last_modified']))
+        click.echo(u"Wrote {} row(s) to {}"
+                    .format(len(uploads), output_path))
 
-    if uploads_missing_resources:
-        click.echo(uploads_missing_resources)
 
-    click.echo(u"Found {} upload(s) with missing or deleted resources."
-                .format(len(uploads_missing_resources)))
+def _list_uploads(output_path):
+    good_uploads = _get_uploads()
+
+    if output_path:
+        _write_uploads_to_csv(output_path, good_uploads)
+    else:
+        click.echo(u"Found {} resource(s)."
+                    .format(len(good_uploads)))
+
+
+def _list_unlinked_uploads(output_path):
+    uploads_missing_resources = _get_uploads(get_linked = False)
+
+    if output_path:
+        _write_uploads_to_csv(output_path, uploads_missing_resources)
+    else:
+        click.echo(u"Found {} upload(s) with missing or deleted resources."
+                    .format(len(uploads_missing_resources)))
 
 
 def _remove_unlinked_uploads():
     cs = CloudStorage()
 
-    uploads_missing_resources = _get_unlinked_uploads(return_objects = True)
+    uploads_missing_resources = _get_uploads(get_linked = False, return_upload_objects_only = True)
 
     num_success = 0
     num_failures = 0
@@ -236,32 +315,45 @@ def _remove_unlinked_uploads():
         click.echo(u"Failed to delete {} upload(s).".format(num_failures))
 
 
-def _list_missing_uploads():
+def _list_missing_uploads(output_path):
     cs = CloudStorage()
 
     upload_urls = set(u.name for u in cs.container.list_objects())
 
-    resource_ids_and_filenames = model.Session.query(
-                                    model.Resource.id,
-                                    model.Resource.url) \
-                                 .filter(model.Resource.url_type == u'upload') \
-                                 .all()
+    resource_fields = model.Session.query(
+                        model.Resource.id,
+                        model.Resource.url,
+                        model.Resource.package_id,
+                        model.Resource.created,
+                        model.Resource.last_modified,
+                        model.Package.owner_org) \
+                        .join(model.Package, model.Resource.package_id == model.Package.id) \
+                        .filter(model.Resource.url_type == u'upload') \
+                        .all()
 
-    resource_ids_missing_uploads = []
-    for id, filename in resource_ids_and_filenames:
+    resources_missing_uploads = []
+    for id, filename, package_id, created, last_modified, organization_id in resource_fields:
         url = os.path.join(
-                'resources',
+                u'resources',
                 id,
                 munge_filename(filename))
 
         if url not in upload_urls:
-            resource_ids_missing_uploads.append(id)
+            resources_missing_uploads.append({
+                u'resource_idid': id,
+                u'resource_filename': filename,
+                u'package_id': package_id,
+                u'created': h.render_datetime(created),
+                u'last_modified': h.render_datetime(last_modified),
+                u'organization_id': organization_id,
+                u'upload_url': None,
+                u'upload_size': None})
 
-    if resource_ids_missing_uploads:
-        click.echo(resource_ids_missing_uploads)
-
-    click.echo(u"Found {} resource(s) with missing uploads."
-                .format(len(resource_ids_missing_uploads)))
+    if output_path:
+        _write_uploads_to_csv(output_path, resources_missing_uploads)
+    else:
+        click.echo(u"Found {} resource(s) with missing uploads."
+                    .format(len(resources_missing_uploads)))
 
 
 def _initdb():
